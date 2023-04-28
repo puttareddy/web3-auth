da941b765fdde837d8d2ac27353ba58c6b6efde3
puttareddy <puttaiaharugunta@Puttaiahs-MacBook-Pro.local>
2023-04-27 22:18:42 -0400

    updated with get message

--- server/index.js
+++ server/index.js
@@ -5,12 +5,24 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+// Generate a message that needs to be signed by the user
+const message = "Please sign this message to authenticate with the server.";
+
+// Endpoint for authentication
+app.get("/authenticate", (req, res) => {
+  try {
+    // Send the message to the client
+    res.json({ message });
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+});
 
 app.post('/authenticate', async (req, res) => {
   const { account, signature } = req.body;
   console.log('account--'+account)
   console.log('signature--'+signature)
-  const message = 'Please sign this message to authenticate with the server.';
   const recoveredAddress = ethers.utils.verifyMessage(message, signature);
 
   if (recoveredAddress.toLowerCase() === account.toLowerCase()) {
